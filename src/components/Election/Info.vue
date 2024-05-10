fbc298b0786e881813ad85769abd4916c4bdb86f
B.Oyuntuya <53362683+oyuntuyab@users.noreply.github.com>
2024-05-10 15:49:58 +0800

    update ui in election info page

--- src/components/Election/Info.vue
+++ src/components/Election/Info.vue
@@ -1,143 +1,57 @@
 <template>
-  <div class="crm-on-mapstyles__CRMOnMapOverlay-sc-1t1jutn-0 iDOdkd">
-    <div class="core-result-generalsstyles__OuterContainer-sc-1t46y8j-0 LLaaP">
-      <div color="#FF4A43" class="core-result-generalsstyles__WinningTab-sc-1t46y8j-3 iHPAGD"></div>
-      <div class="core-result-generalsstyles__Container-sc-1t46y8j-1 gmVuGx">
-        <div class="core-result-generalsstyles__ContainerInner-sc-1t46y8j-2 ixHOBW">
-          <div class="core-result-generals-headerstyles__CRMGHeader-iinac4-0 crZsYa">
-            <div class="core-result-generals-headerstyles__HeaderSide-iinac4-1 jGaOtC">
-              <div class="core-result-generals-headerstyles__StateInfoWrapper-iinac4-2 hxnmRO">
-                <div class="core-result-generals-headerstyles__StateInfoWrapperLeft-iinac4-3 cLGfYg">
-                  <div class="core-result-generals-headerstyles__WrapTogether-iinac4-5 gPwQqV">
-                    <div class="core-result-generals-headerstyles__RaceName-iinac4-6 gGlTLu"> President: Wyoming
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="core-result-generals-headerstyles__CRMGSubtitle-iinac4-8 zzkFV">
-                <div class="core-result-generals-headerstyles__FFSCrmElectoralVotes-iinac4-10 jdHWbA">3 electoral
-                  votes
-                </div>
-              </div>
+  <div>
+    <div class="relative shadow rounded-lg bg-white">
+      <div class="absolute top-0 h-2 w-full bg-red-500 rounded-t-lg"></div>
+      <div class="p-4">
+
+        <div>
+          <div class="pt-4 pb-2 flex justify-between space-x-10 ">
+            <div class="">
+              <span class="text-base font-semibold text-gray-800">President: Oregon</span>
+              <p>7 Electoral Votes</p>
             </div>
-            <div class="core-result-generals-headerstyles__ProjectedWinnerSide-iinac4-11 gfHcHg">
-              <div class="core-result-generals-headerstyles__FFSRWinnerContainer-iinac4-13 jdFNnD">
-                <div color="#FF4A43" class="core-result-generals-headerstyles__Winner-iinac4-14 ilniXY">Trump
-                </div>
-                <span class="core-result-generals-headerstyles__CheckmarkWrapper-iinac4-15 cdGUre"><svg width="16"
-                                                                                                        height="16"
-                                                                                                        viewBox="0 0 16 16"
-                                                                                                        version="1.1"
-                                                                                                        xmlns="http://www.w3.org/2000/svg"><title>icon / checkmark</title><g
-                  id="icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g
-                  id="icon-/-checkmark--/-dem"><path
-                  d="M0,8 C0,12.4181333 3.58186667,16 8,16 C12.4181333,16 16,12.4181333 16,8 C16,3.5816 12.4181333,0 8,0 C3.58186667,0 0,3.5816 0,8 Z"
-                  id="path-1-copy" fill="#FF4A43"></path><path
-                  d="M13.5967059,5.98070588 L8.248,11.3294118 L7.24329412,12.3341176 C7.10541176,12.472 6.93788235,12.5407059 6.74117647,12.5407059 C6.544,12.5407059 6.37647059,12.472 6.23858824,12.3341176 L5.23435294,11.3294118 L2.55952941,8.65505882 C2.42164706,8.51717647 2.35294118,8.34964706 2.35294118,8.15294118 C2.35294118,7.95576471 2.42164706,7.78823529 2.55952941,7.65035294 L3.56470588,6.64564706 C3.70258824,6.50823529 3.86964706,6.43905882 4.06682353,6.43905882 C4.264,6.43905882 4.43105882,6.50823529 4.56894118,6.64564706 L6.74117647,8.82494118 L11.5872941,3.97129412 C11.7247059,3.83341176 11.8922353,3.76470588 12.0894118,3.76470588 C12.2865882,3.76470588 12.4541176,3.83341176 12.592,3.97129412 L13.5967059,4.976 C13.7345882,5.11435294 13.8032941,5.28188235 13.8032941,5.47858824 C13.8032941,5.67529412 13.7345882,5.84282353 13.5967059,5.98070588 Z"
-                  id="Fill-1" fill="#FFFFFF"></path></g></g></svg></span></div>
-              <div color="#FF4A43" class="core-result-generals-headerstyles__FFSRProjectedWinner-iinac4-12 KlVaL">
-                projected winner
+            <div class="space-y-1 text-right text-blue-500">
+              <div class="">
+                <span class="text-base font-bold">Biden</span>
+                <p>7 Electoral Votes</p>
               </div>
-              <div class="follow-this-racestyles__FollowThisRaceContent-ozeam6-5 eekZEV">
-                <div
-                  class="follow-this-racestyles__RaceButtonBase-ozeam6-0 follow-this-racestyles__FollowButton-ozeam6-2 dOIfHF">
-                  + Follow
-                </div>
-              </div>
+              <a-button size="small" shape="round">
+                <template #icon> + </template>
+                <span class="text-[10px] font-bold uppercase">Follow</span>
+              </a-button>
             </div>
           </div>
-          <table class="core-result-generals-tablestyles__Table-sc-1d02ybh-0 datsGf">
-            <thead class="core-result-generals-tablestyles__Thead-sc-1d02ybh-4 dmvnun">
-            <tr class="core-result-generals-tablestyles__Tr-sc-1d02ybh-3 hPoGgN">
-              <th
-                class="core-result-generals-tablestyles__Th-sc-1d02ybh-1 core-result-generals-tablestyles__ThName-sc-1d02ybh-6 iAgNGg">
-                Candidate
-              </th>
-              <th data-testid="gcrm-percentage-header"
-                  class="core-result-generals-tablestyles__Th-sc-1d02ybh-1 core-result-generals-tablestyles__FFSRThPercent-sc-1d02ybh-8 dQNHZM">
-                %
-              </th>
-              <th data-testid="gcrm-vote-header"
-                  class="core-result-generals-tablestyles__Th-sc-1d02ybh-1 core-result-generals-tablestyles__FFSRThVotes-sc-1d02ybh-12 jFutWo">
-                Votes
-              </th>
-            </tr>
-            </thead>
-            <tbody data-testid="gcrm-table-body"
-                   class="core-result-generals-tablestyles__Tbody-sc-1d02ybh-5 llSLVh">
-            <tr class="core-result-generals-tablestyles__Tr-sc-1d02ybh-3 eMassY">
-              <td
-                class="core-result-generals-tablestyles__Td-sc-1d02ybh-2 core-result-generals-tablestyles__TdName-sc-1d02ybh-7 jeeVBj">
-                <div class="core-result-generals-tablestyles__FlexWrapper-sc-1d02ybh-15 RlMPC">
-                  <div class="core-result-generals-tablestyles__CandidateContentWrapper-sc-1d02ybh-16 bbYtmu">
-                    <div color="#FF4A43"
-                         class="core-result-generals-tablestyles__PartyDot-sc-1d02ybh-14 eGqrJN"></div>
-                    <div class="core-result-generals-tablestyles__Name-sc-1d02ybh-17 fIbwoL"><span>Trump</span>
-                      <div class="core-result-generals-tablestyles__Incumbent-sc-1d02ybh-18 lhjspJ"></div>
-                    </div>
-                  </div>
-                  <div class="core-result-generals-tablestyles__FFSRTableCheckmarkWrapper-sc-1d02ybh-23 gHXDU">
-                    <svg width="16" height="16" viewBox="0 0 16 16" version="1.1"
-                         xmlns="http://www.w3.org/2000/svg"><title>icon / checkmark</title>
-                      <g id="icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <g id="icon-/-checkmark--/-dem">
-                          <path
-                            d="M0,8 C0,12.4181333 3.58186667,16 8,16 C12.4181333,16 16,12.4181333 16,8 C16,3.5816 12.4181333,0 8,0 C3.58186667,0 0,3.5816 0,8 Z"
-                            id="path-1-copy" fill="#FF4A43"></path>
-                          <path
-                            d="M13.5967059,5.98070588 L8.248,11.3294118 L7.24329412,12.3341176 C7.10541176,12.472 6.93788235,12.5407059 6.74117647,12.5407059 C6.544,12.5407059 6.37647059,12.472 6.23858824,12.3341176 L5.23435294,11.3294118 L2.55952941,8.65505882 C2.42164706,8.51717647 2.35294118,8.34964706 2.35294118,8.15294118 C2.35294118,7.95576471 2.42164706,7.78823529 2.55952941,7.65035294 L3.56470588,6.64564706 C3.70258824,6.50823529 3.86964706,6.43905882 4.06682353,6.43905882 C4.264,6.43905882 4.43105882,6.50823529 4.56894118,6.64564706 L6.74117647,8.82494118 L11.5872941,3.97129412 C11.7247059,3.83341176 11.8922353,3.76470588 12.0894118,3.76470588 C12.2865882,3.76470588 12.4541176,3.83341176 12.592,3.97129412 L13.5967059,4.976 C13.7345882,5.11435294 13.8032941,5.28188235 13.8032941,5.47858824 C13.8032941,5.67529412 13.7345882,5.84282353 13.5967059,5.98070588 Z"
-                            id="Fill-1" fill="#FFFFFF"></path>
-                        </g>
-                      </g>
-                    </svg>
-                  </div>
-                </div>
-              </td>
-              <td data-testid="gcrm-percentage-cell"
-                  class="core-result-generals-tablestyles__Td-sc-1d02ybh-2 core-result-generals-tablestyles__FFSRTdPercent-sc-1d02ybh-9 fLtFjI">
-                69.9%
-              </td>
-              <td data-testid="gcrm-vote-cell"
-                  class="core-result-generals-tablestyles__Td-sc-1d02ybh-2 core-result-generals-tablestyles__FFSRTdVotes-sc-1d02ybh-13 gwqxFH">
-                193,559
-              </td>
-            </tr>
-            <tr class="core-result-generals-tablestyles__Tr-sc-1d02ybh-3 eMassY">
-              <td
-                class="core-result-generals-tablestyles__Td-sc-1d02ybh-2 core-result-generals-tablestyles__TdName-sc-1d02ybh-7 jeeVBj">
-                <div class="core-result-generals-tablestyles__FlexWrapper-sc-1d02ybh-15 ebLWUP">
-                  <div class="core-result-generals-tablestyles__CandidateContentWrapper-sc-1d02ybh-16 bbYtmu">
-                    <div color="#1A6AFF"
-                         class="core-result-generals-tablestyles__PartyDot-sc-1d02ybh-14 gGmoMo"></div>
-                    <div class="core-result-generals-tablestyles__Name-sc-1d02ybh-17 fIbwoL"><span>Biden</span>
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td data-testid="gcrm-percentage-cell"
-                  class="core-result-generals-tablestyles__Td-sc-1d02ybh-2 core-result-generals-tablestyles__FFSRTdPercent-sc-1d02ybh-9 fLtFjI">
-                26.6%
-              </td>
-              <td data-testid="gcrm-vote-cell"
-                  class="core-result-generals-tablestyles__Td-sc-1d02ybh-2 core-result-generals-tablestyles__FFSRTdVotes-sc-1d02ybh-13 gwqxFH">
-                73,491
-              </td>
-            </tr>
-            </tbody>
-          </table>
         </div>
-        <div class="core-result-generals-footerstyles__Footer-sc-1c6sqkf-0 hnNOKL">
-          <div class="core-result-generals-footerstyles__Updates-sc-1c6sqkf-1 eULQic">
-            <div class="core-result-generals-footerstyles__UpdateInProgress-sc-1c6sqkf-8 ekmRNX">
-              <div class="core-result-generals-footerstyles__FFSREst-sc-1c6sqkf-6 lfbFtM">Est. 99% In</div>
-            </div>
-            <div class="core-result-generals-footerstyles__FFSRLastUpdated-sc-1c6sqkf-3 gZesqy">Updated 08:22 a.m.
-              ET, Nov. 24
-            </div>
+
+        <div class="">
+          <div class="py-2 sm:grid sm:grid-cols-4 sm:gap-4 text-gray-500">
+            <dt class="sm:col-span-2">Candidate</dt>
+            <dd class="sm:mt-0 sm:col-span-1 text-right">%</dd>
+            <dd class="sm:mt-0 sm:col-span-1 text-right">Votes</dd>
           </div>
-          <div class="core-result-generals-footerstyles__FullStateDetailsWrapper-sc-1c6sqkf-9 eJmvkm"><a
-            href="/election/2020/results/state/wyoming/president"
-            class="core-result-generals-footerstyles__FullStateDetails-sc-1c6sqkf-5 gQmmQx">Full Details</a></div>
+          <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+            <dl class="sm:divide-y sm:divide-gray-200 border-b border-gray-200">
+              <div class="py-1 sm:grid sm:grid-cols-4 sm:gap-4">
+                <dt class="text-gray-500 sm:col-span-2">Biden</dt>
+                <dd class="dark:text-white sm:mt-0 sm:col-span-1 text-right">56.5%</dd>
+                <dd class="dark:text-white sm:mt-0 sm:col-span-1 text-right">1,340,383</dd>
+              </div>
+              <div class="py-1 sm:grid sm:grid-cols-4 sm:gap-4">
+                <dt class="text-gray-500 sm:col-span-2">Candidate</dt>
+                <dd class="dark:text-white sm:mt-0 sm:col-span-1 text-right">56.5%</dd>
+                <dd class="dark:text-white sm:mt-0 sm:col-span-1 text-right">1,340,383</dd>
+              </div>
+            </dl>
+          </div>
+
+
+          <div class="pt-4 flex justify-between space-x-10 ">
+            <div class="text-xs">
+              <span class="font-semibold text-gray-500">Est. 99% In</span>
+              <p >Updated 08:37 p.m. ET, Dec. 5</p>
+            </div>
+            <a-button shape="round" >Full details</a-button>
+          </div>
         </div>
       </div>
     </div>
